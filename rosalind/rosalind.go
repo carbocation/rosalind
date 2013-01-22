71d5917a7a099711038e0ddd24e283ce623d43d2
James Pirruccello <james@parchment.com>
2013-01-22 01:24:18 -0500

    Added Rosalind Problem #3

--- rosalind/rosalind.go
+++ rosalind/rosalind.go
@@ -25,3 +25,41 @@ func TranscribeDna(Dna []byte) []byte {
 
     return Rna
 }
+
+func ReverseComplement(Dna []byte) []byte {
+    revc := reverseBytes(Dna)
+
+    for i, base := range revc {
+        revc[i] = complementDna(base)
+    }
+    
+    return revc
+}
+
+//Non-exported functions, i.e., functions that simply support the primary functions in this package
+
+func complementDna(base byte) byte {
+    switch{
+        case base == 'A':
+            return 'T'
+        case base == 'T':
+            return 'A'
+        case base == 'C':
+            return 'G'
+        case base == 'G':
+            return 'C'
+    }
+
+    return 0
+}
+
+func reverseBytes(a []byte) []byte {
+    b := make([]byte, len(a))
+    j := len(b) - 1
+
+    for i := 0; i <= j; i++ {
+        b[j-i] = a[i]
+    }
+
+    return b
+}
