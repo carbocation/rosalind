8c869df2388246a0fdccd69cc79b64957e3d4eb2
James Pirruccello <james@parchment.com>
2013-01-22 18:37:13 -0500

    Moved queue and stack to own library. Moved the non-exported methods to own file.

--- rosalind/rosalind.go
+++ rosalind/rosalind.go
@@ -35,31 +35,3 @@ func ReverseComplement(Dna []byte) []byte {
     
     return revc
 }
-
-//Non-exported functions, i.e., functions that simply support the primary functions in this package
-
-func complementDna(base byte) byte {
-    switch{
-        case base == 'A':
-            return 'T'
-        case base == 'T':
-            return 'A'
-        case base == 'C':
-            return 'G'
-        case base == 'G':
-            return 'C'
-    }
-
-    return 0
-}
-
-func reverseBytes(a []byte) []byte {
-    b := make([]byte, len(a))
-    j := len(b) - 1
-
-    for i := 0; i <= j; i++ {
-        b[j-i] = a[i]
-    }
-
-    return b
-}
